2adc019328f1d89281b434653ee9d39249013a38
John Esther <estaarjohn@gmail.com>
2022-09-09 12:31:45 +0300

    Add browser headers for beautiful soup

--- census_downloader.py
+++ census_downloader.py
@@ -4,3 +4,13 @@ from bs4 import BeautifulSoup
 import requests
 from random import randint
 from time import sleep
+
+# Add Headers for beautiful soup to imitate a browser
+
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+}
