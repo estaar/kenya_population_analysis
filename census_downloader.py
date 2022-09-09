3d595deb4baf669f8ba4ca5bd6593b2ea8dc018d
John Esther <estaarjohn@gmail.com>
2022-09-09 12:39:31 +0300

    Created a function to download csv files

--- census_downloader.py
+++ census_downloader.py
@@ -14,3 +14,20 @@ headers = {
     'Access-Control-Max-Age': '3600',
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 }
+
+# Create a function to crawl through the site and download a population csvs
+def main(URL):
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    for item in soup.find_all("a", class_="resource-url-analytics"):
+        url = item.get('href')
+        if url.endswith(".csv"):
+            print(url)
+            name = url.rsplit("/", 1)[-1]
+            print(f"Downloading {name}")
+            csv_data = requests.get(url)
+            name = url.rsplit("/", 1)[-1]
+            with open(name, 'wb') as f:
+                f.write(csv_data.content)
+        sleep(randint(10,30))
+
